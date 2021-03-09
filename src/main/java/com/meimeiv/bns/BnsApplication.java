3bfafa9114fc1131ddc73ed30be3199f1f5a5ae3
Vzifeng <1281084456@qq.com>
2021-03-09 10:06:20 +0800

    first commit

---
+++ src/main/java/com/meimeiv/bns/BnsApplication.java
@@ -0,0 +1,19 @@
+package com.meimeiv.bns;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+@MapperScan(basePackages = "com.meimeiv.bns.dao")
+@ComponentScan(basePackages = {"com.meimeiv.bns"})
+@Configuration
+public class BnsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BnsApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/meimeiv/bns/po/User.java
@@ -0,0 +1,73 @@
+package com.meimeiv.bns.po;
+
+public class User {
+    private Integer id;
+
+    private String userName;
+
+    private Boolean userSex;
+
+    private String userPhone;
+
+    private String userAddr;
+
+    private String userRegistrationTime;
+
+    private Integer departmentId;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName == null ? null : userName.trim();
+    }
+
+    public Boolean getUserSex() {
+        return userSex;
+    }
+
+    public void setUserSex(Boolean userSex) {
+        this.userSex = userSex;
+    }
+
+    public String getUserPhone() {
+        return userPhone;
+    }
+
+    public void setUserPhone(String userPhone) {
+        this.userPhone = userPhone == null ? null : userPhone.trim();
+    }
+
+    public String getUserAddr() {
+        return userAddr;
+    }
+
+    public void setUserAddr(String userAddr) {
+        this.userAddr = userAddr == null ? null : userAddr.trim();
+    }
+
+    public String getUserRegistrationTime() {
+        return userRegistrationTime;
+    }
+
+    public void setUserRegistrationTime(String userRegistrationTime) {
+        this.userRegistrationTime = userRegistrationTime == null ? null : userRegistrationTime.trim();
+    }
+
+    public Integer getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(Integer departmentId) {
+        this.departmentId = departmentId;
+    }
+}
